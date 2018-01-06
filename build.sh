05f32d6931af8eb80ba0854e6e7c9add1ac7ecbe
Martin <martinnagelberg@gmail.com>
2018-01-06 02:32:29 -0300

    aS

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+echo "buildeando esta pija"
 cd ./src/server
+echo "go get"
 go get
+echo "la concha de tu vieja"
 make install
+echo "lo hice"
--- src/server/main.go
+++ src/server/main.go
@@ -37,6 +37,6 @@ func main() {
 	app.Run(iris.TLS("localhost:7668", "certs/mycert.cert", "certs/mykey.key"))
 	*/
 
-	app.Run(iris.Addr(":80"))
+	app.Run(iris.Addr(os.Getenv("PORT")))
 
 }
