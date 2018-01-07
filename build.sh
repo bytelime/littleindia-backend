5908cde8200ba94b06b496155ca9c93ca26be8af
Martin <martinnagelberg@gmail.com>
2018-01-06 23:27:20 -0300

    asd

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-cd /var/app/staging/src/server
 go get
-go build -o ./../../bin/server
+go build -o /bin/server
---
+++ src/server/build.sh
@@ -0,0 +1,3 @@
+cd /var/app/staging/src/server
+go get
+go build -o ./../../bin/server
