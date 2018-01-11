dc40e853bfaf8ed96fa983d3913448fa24334163
Ezequiel <elynch@itba.edu.ar>
2018-01-11 17:20:23 -0300

    removed unnecesary select (changed keys in db)

--- src/server/persistence/categoryPersistence.go
+++ src/server/persistence/categoryPersistence.go
@@ -2,6 +2,8 @@ package persistence
 
 import (
 	CO "server/config"
+	"log"
+  "github.com/go-sql-driver/mysql"
 )
 
 func AddCategory(name string) error {
@@ -9,17 +11,25 @@ func AddCategory(name string) error {
 	db := DB()
 	defer db.Close()
 
-	var categoryCount int
-
-	db.QueryRow("SELECT COUNT(id) AS categoryCount FROM Categories WHERE name=?", name).Scan(&categoryCount)
-
-	if (categoryCount > 0){
-		return CO.ThrowError("alreadyExists")
-	}
-
 	stmt, _ := db.Prepare("INSERT INTO Categories(name) VALUES(?)")
-	_, err := stmt.Exec(name)
-	
+	res , err := stmt.Exec(name)
+
+	//se puede poner en una fc aparte cuando haya mas de un tipo de error y se use en distintas clases
+	if err != nil {
+	  me, ok := err.(*mysql.MySQLError)
+	  if !ok {
+      return err
+    }
+    if me.Number == 1062 { //errno de mysql para duplicate key
+      return CO.ThrowError("alreadyExists")
+    }
+	  return err
+  }
+
+  //devuelve el id con el q agrego la ultima cosa q agrego
+	lastId, err := res.LastInsertId()
+	log.Printf("%d", lastId)
+
 	return err
 
 }
@@ -29,8 +39,8 @@ func RemoveCategory(name string) error{
 	db := DB()
 	defer db.Close()
 
-	_, err := db.Exec("DELETE FROM Categories WHERE name=?", name)
-	
+	_ , err := db.Exec("DELETE FROM Categories WHERE name=?", name)
+
 	return err
 
 }
